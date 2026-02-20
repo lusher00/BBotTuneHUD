46ca85614c6c82f07ce8c694b5848b138d84e3c1
Ryan Lush <ryan.lush@gmail.com>
2026-02-20 15:09:33 -0500

    Initial Commit

---
+++ CatFollowerApp/CatFollowerAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CatFollowerAppApp.swift
+//  CatFollowerApp
+//
+//  Created by rlush on 2/20/26.
+//
+
+import SwiftUI
+
+@main
+struct CatFollowerAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
